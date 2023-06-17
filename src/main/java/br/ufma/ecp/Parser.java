2d5f0ec9a76af7d61fb4db9bdf091c3a8f977f05
Your Name <you@example.com>
2023-06-17 13:59:13 -0300

    Testando literais strings

--- src/main/java/br/ufma/ecp/Parser.java
+++ src/main/java/br/ufma/ecp/Parser.java
@@ -357,7 +357,13 @@ public class Parser {
 
             case STRING:
                 expectPeek(TokenType.STRING);
-                var strValue = currentToken.lexeme; 
+                var strValue = currentToken.lexeme;
+                vmWriter.writePush(Segment.CONST, strValue.length());
+                vmWriter.writeCall("String.new", 1);
+                for (int i = 0; i < strValue.length(); i++) {
+                    vmWriter.writePush(Segment.CONST, strValue.charAt(i));
+                    vmWriter.writeCall("String.appendChar", 2);
+                }
                 break;
             
 
